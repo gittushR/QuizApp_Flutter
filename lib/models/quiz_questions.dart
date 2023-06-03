48140aa9c273ce2b28ad7ce27e50e8dd05cbca20
tushar Rathi <tusharrathi2021@outlook.com>
2023-06-03 09:42:22 +0545

    Created Shuffled lists for answers

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -3,4 +3,10 @@ class QuizQuestions {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    var shuffledAnswers = List.of(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -14,20 +14,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
     final currentQuestion = questions[0];
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          AnswerButton(currentQuestion.answers[0], () {}),
-          AnswerButton(currentQuestion.answers[1], () {}),
-          AnswerButton(currentQuestion.answers[2], () {}),
-          AnswerButton(currentQuestion.answers[3], () {}),
-        ],
+    return Container(
+      margin: const EdgeInsets.all(50),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, () {});
+            }),
+          ],
+        ),
       ),
     );
   }
